806f9b755c84b6fd9edacaffa5eca03493d6f9d1
Arean Narrayan <2018HT66528@wilp.bits-pilani.ac.in>
2020-04-12 03:38:37 +0530

    updated changes 2

--- MY_FILE.sh
+++ MY_FILE.sh
@@ -10,6 +10,6 @@ echo "Welcome to BITS Pilani";
 echo -e "Welcome to BITS Pilani Course\nI have taken DevOps as an elective\nCourse ID:CSI-ZG514 ";
 echo -e "\n\nThis is first GIT project"
 echo -e "Changes through Git bash"
-echo -e "\nThis project was initiated by Arean Narrayan"
+echo -e "\nThis project was initiated by AREAN NARRAYAN"
 
 
