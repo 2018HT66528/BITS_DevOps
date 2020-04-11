a0d375d1412aa24f6c3be6d5eb5bf855e52955d8
Arean Narrayan <2018HT66528@wilp.bits-pilani.ac.in>
2020-04-12 03:17:35 +0530

    First Commit

---
+++ MY_FILE.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+################################################################################################################
+# Name: Arean Narrayan
+# Title: START FILE
+# Description: This is the first script
+#
+################################################################################################################
+echo "Welcome to BITS Pilani";
+
+echo -e "Welcome to BITS Pilani Course\nI have taken DevOps as an elective\nCourse ID:CSI-ZG514 ";
+echo -e "This is first GIT project"
+echo -e "Changes through Git bash"
+
+
