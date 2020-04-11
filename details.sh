8d0544bbd1ff920d2b0c0a8c5e5a333c9c9584c5
Arean Narrayan <2018HT66528@wilp.bits-pilani.ac.in>
2020-04-12 04:42:33 +0530

    added details

---
+++ details.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+################################################################################################################
+# Name: Arean Narrayan
+# Title: details
+################################################################################################################
+
+
+echo "Today's date is:"; date
+echo  "\nCurrent Workign Directory is :"; pwd
+echo  "\nMy System Name is:"; hostname
+echo  "\nAll above commands are run by user:"; whoami
