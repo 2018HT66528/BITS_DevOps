6b5192779921787276e3c2ec64452c3f84a297ca
Arean Narrayan <2018HT66528@wilp.bits-pilani.ac.in>
2020-04-13 05:28:32 +0530

    added a file NAME

---
+++ NAME.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+################################################################################################################
+# Name: Arean NArrayan
+# Description: This script is read two inputs :
+# First Name & Last Name
+################################################################################################################
+
+echo "Please Enter you First Name:"; # Display Message to User
+read firstname; # Read input first name from User
+echo "Please Enter your Last Name:"; # Display Message to User
+read lastname; # Read input as last name from User
+echo "Here is your full name: $firstname $lastname" # Display the inputs with proper statement to the User
