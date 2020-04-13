848dc0c448fc43528602c5489f84517966b49399
Arean Narrayan <2018HT66528@wilp.bits-pilani.ac.in>
2020-04-13 14:31:29 +0530

    added new file combined

---
+++ COMBINED.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+################################################################################################################
+# Name: Arean Narrayan
+# Title: Combined file
+# Description: This is the first script
+#
+################################################################################################################
+echo "Welcome to BITS Pilani";
+
+echo  "Welcome to BITS Pilani Course\nI have taken DevOps as an elective\nCourse ID:CSI-ZG514 ";
+echo  "\n\nThis is Test GIT project"
+echo  "Changes through Git bash"
+echo  "\nThis project was initiated by AREAN NARRAYAN"
+echo "\nPlease Enter you First Name:"; # Display Message to User
+read firstname; # Read input first name from User
+echo "Please Enter your Last Name:"; # Display Message to User
+read lastname; # Read input as last name from User
+echo "Here is your full name: $firstname $lastname" # Display the inputs with proper statement to the User
+echo "\nThe current Date and time is:";date
+
+
