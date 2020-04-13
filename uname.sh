cfbcc6740ca838ea6f0ead6527479b07a2636541
Arean Narrayan <2018HT66528@wilp.bits-pilani.ac.in>
2020-04-13 13:07:10 +0530

    added a new file in branch b1

---
+++ uname.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+################################################################################################################
+# Name: Arean Narrayan
+# Title: uname
+#
+# This script displays information about captured systems commands then display it
+################################################################################################################
+
+echo "Today's date is:"; date
+echo "Current Workign Directory is :"; pwd
+echo "My System Name is:"; hostname
+echo "My system details are :"; uname -a
+echo "All above commands are run by user:"; whoami
+
